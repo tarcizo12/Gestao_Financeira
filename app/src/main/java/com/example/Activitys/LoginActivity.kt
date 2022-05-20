8c39b08dc6e27f20aeaa37489273e09f16aa12a2
Tarcizo Neto <tarcizon12@gmail.com>
2022-05-20 13:04:21 -0300

    Inicializando tela de login

--- app/src/main/java/com/example/Activitys/LoginActivity.kt
+++ app/src/main/java/com/example/Activitys/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.gestao_financeira_app
+package com.example.Activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gestao_financeira_app.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_activity)
     }
 }
