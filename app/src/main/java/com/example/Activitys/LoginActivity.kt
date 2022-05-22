3bf38dce02ecff8674a6b2a9eb71bf6b6c1afeba
Tarcizo Neto <tarcizon12@gmail.com>
2022-05-22 18:18:37 -0300

    Direcionando para tela de registro

--- app/src/main/java/com/example/Activitys/LoginActivity.kt
+++ app/src/main/java/com/example/Activitys/LoginActivity.kt
@@ -1,12 +1,48 @@
 package com.example.Activitys
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import java.util.*
 
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var mRegisterButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        mRegisterButton = findViewById(R.id.registerButton)
+        mRegisterButton.setOnClickListener(this)
+
+    }
+
+    override fun onClick(button: View?) {
+
+        val buttonPressed = button?.id
+
+        when(buttonPressed){
+            R.id.registerButton -> openActivity("Register")
+        }
+
+
+    }
+
+    private fun openActivity(activitySelected: String) {
+        var it: Intent
+
+        when(activitySelected){
+            "Register" -> it = Intent(this,RegisterActivity::class.java)
+            else -> it = Intent(this,LoginActivity::class.java)
+        }
+
+        startActivity(it)
     }
 }
+
+
+
+
