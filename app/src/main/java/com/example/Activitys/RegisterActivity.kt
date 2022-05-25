444b8f86feae4ef0783cafcc5cf74bc4948552e5
Davi Ximenes <davixmnsl@gmail.com>
2022-05-25 15:20:17 -0300

    referenciando ids a objetos da activity de register_activity.xml

--- app/src/main/java/com/example/Activitys/RegisterActivity.kt
+++ app/src/main/java/com/example/Activitys/RegisterActivity.kt
@@ -2,10 +2,28 @@ package com.example.Activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var registerName: EditText
+    private lateinit var registerEmail: EditText
+    private lateinit var registerMobileNumber: EditText
+    private lateinit var registerPassword: EditText
+    private lateinit var registerConfirmaPassword: EditText
+    private lateinit var registerRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register_activity)
+
+        registerName = findViewById(R.id.registerName)
+        registerEmail = findViewById(R.id.registerEmail)
+        registerMobileNumber = findViewById(R.id.registerMobileNumber)
+        registerPassword = findViewById(R.id.registerPassword)
+        registerConfirmaPassword = findViewById(R.id.registerConfirmPassword)
+        registerRegister = findViewById(R.id.registerRegister)
+
     }
 }
