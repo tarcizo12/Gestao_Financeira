0ff01a9d45dfda4984f4d50b79516c41455d7896
Tarcizo Neto <tarcizon12@gmail.com>
2022-05-28 14:34:34 -0300

    Verifica campos de registro

--- app/src/main/java/com/example/Activitys/RegisterActivity.kt
+++ app/src/main/java/com/example/Activitys/RegisterActivity.kt
@@ -1,19 +1,21 @@
 package com.example.Activitys
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import kotlin.math.log
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var registerName: EditText
     private lateinit var registerEmail: EditText
     private lateinit var registerMobileNumber: EditText
     private lateinit var registerPassword: EditText
-    private lateinit var registerConfirmaPassword: EditText
-    private lateinit var registerRegister: Button
+    private lateinit var registerConfirmPassword: EditText
+    private lateinit var registerButtonConfirm: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +25,65 @@ class RegisterActivity : AppCompatActivity() {
         registerEmail = findViewById(R.id.registerEmail)
         registerMobileNumber = findViewById(R.id.registerMobileNumber)
         registerPassword = findViewById(R.id.registerPassword)
-        registerConfirmaPassword = findViewById(R.id.registerConfirmPassword)
-        registerRegister = findViewById(R.id.registerRegister)
+        registerConfirmPassword = findViewById(R.id.registerConfirmPassword)
+        registerButtonConfirm = findViewById(R.id.registerButtonConfirm)
+
+        registerButtonConfirm.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+
+        val email = registerEmail.text.toString().trim()
+        val name = registerName.text.toString().trim()
+        val number = registerMobileNumber.text.toString().trim()
+        val password = registerPassword.text.toString().trim()
+        val confirmPassword = registerConfirmPassword.text.toString().trim()
+
+        if(validateData(email, name, number, password, confirmPassword)) registerUser()
+
+    }
+
+    private fun registerUser() {
+        TODO("Not yet implemented")
+    }
+
+    private fun validateData(
+        email: String,
+        name: String,
+        number: String,
+        password: String,
+        confirmPassword: String
+    ): Boolean {
+
+        var confirmedData = true
+
+        if (email.isEmpty()) {
+            registerEmail.error = "Coloque um email válido"
+            confirmedData = false
+        }
+        if (name.isEmpty()) {
+            registerName.error = "Coloque um nome válido"
+            confirmedData = false
+        }
+        if (number.isEmpty()) {
+            registerMobileNumber.error = "Coloque um número válido"
+            confirmedData = false
+        }
+        if (password.isEmpty()) {
+            registerPassword.error = "Coloque uma senha válida"
+            confirmedData = false
+        }
+        if (confirmPassword.isEmpty()) {
+            registerConfirmPassword.error = "Coloque uma confirmação válida"
+            confirmedData = false
+        }
+        if (password != confirmPassword) {
+            registerConfirmPassword.error = "Os campos de senha devem ser iguais"
+            confirmedData = false
+        }
+
+        return confirmedData
+
     }
 
 
